1e40de45e22624a198aca7324e22094db07fc480
Uvaish <uvaishnasir@gmail.com>
2025-07-20 21:48:18 +0530

    tailwind setup

--- src/services/coinAPI.js
+++ src/services/coinAPI.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchCoins = async () => {
   const { data } = await axios.get(`${API_BASE_URL}/coins`);
